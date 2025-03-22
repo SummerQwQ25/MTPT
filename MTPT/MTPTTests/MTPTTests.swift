cd1cc610c80b5671279d30d7a30316234b12fe78
QwQ2025 <162947336+SummerQwQ25@users.noreply.github.com>
2025-03-23 07:53:06 +0800

    Add files via upload

---
+++ MTPT/MTPTTests/MTPTTests.swift
@@ -0,0 +1,17 @@
+//
+//  MTPTTests.swift
+//  MTPTTests
+//
+//  Created by yan zheng on 2025/3/23.
+//
+
+import Testing
+@testable import MTPT
+
+struct MTPTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
